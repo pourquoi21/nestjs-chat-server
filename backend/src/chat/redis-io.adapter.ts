a3d51c001f201670114c86ebcee6b7aab5bb0b08
pourquoi21 <miyaku219@naver.com>
2026-08-08 17:14:55 +0900

    fix: redis-io.adapter URL변수 오타 수정

--- backend/src/chat/redis-io.adapter.ts
+++ backend/src/chat/redis-io.adapter.ts
@@ -16,7 +16,7 @@ export class RedisIoAdapter extends IoAdapter {
 
     // Pub(방송하는 사람)과 Sub(듣는 사람) 클라이언트를 각각 만듦
     const pubClient = createClient({
-      url: 'redisUrl',
+      url: redisUrl,
     });
     const subClient = pubClient.duplicate();
 
